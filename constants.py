273694f26cb3f5206ccbaa71dd89556cb2697ab5
Tom K Pace <50508981+tomkpace@users.noreply.github.com>
2021-04-15 21:20:42 -0600

    Fine tuned relation extractor

---
+++ constants.py
@@ -0,0 +1,8 @@
+ner_relations = {
+    "PER": "features the person",
+    "ORG": "features the organization",
+    "LOC": "features the location",
+    "MISC": "features the miscellaneous theme",
+}
+
+removal_values = ("film", "movie", "-")
--- relation_extractor.py
+++ relation_extractor.py
@@ -1,4 +1,22 @@
 from nlp import nlp, ner, process_ner_output
+from constants import ner_relations, removal_values
+
+
+def find_composite_noun_chunk(noun_chunk):
+    composite_noun_chunk_idx = set()
+    composite_noun_chunk = []
+    for n in noun_chunk.doc.noun_chunks:
+        if n == noun_chunk:
+            for t in n:
+                composite_noun_chunk_idx.add(t.i)
+        elif n.root.head.head == noun_chunk.root:
+            for t in n:
+                composite_noun_chunk_idx.add(t.i)
+            composite_noun_chunk_idx.add(n.root.head.i)
+    for t in noun_chunk.doc:
+        if t.i in composite_noun_chunk_idx:
+            composite_noun_chunk.append(t)
+    return composite_noun_chunk
 
 
 def negation_detector(token):
@@ -28,6 +46,29 @@ def verb_extractor(token):
         return None
 
 
+def noun_chunk_ent_detector(noun_chunk):
+    for t in noun_chunk:
+        if t.ent_type_ != "":
+            return True
+    return False
+
+
+def stop_phrase_detector(text):
+    for t in nlp(text):
+        if t.is_stop:
+            continue
+        return False
+    return True
+
+
+def removal_word_detector(text, removal_values=removal_values):
+    for t in nlp(text):
+        if t.is_stop or t.text.lower() in removal_values:
+            continue
+        return False
+    return True
+
+
 class RelationExtractor:
     def __init__(self, entity_id, text):
         self.entity_id = entity_id
@@ -51,7 +92,7 @@ class RelationExtractor:
         processed_ner_output = process_ner_output(ner_output)
         relations = []
         for r in processed_ner_output:
-            relation = (entity_id, "features", r[0])
+            relation = (entity_id, ner_relations[r[1]], r[0])
             relations.append(relation)
         return relations
 
@@ -61,11 +102,14 @@ class RelationExtractor:
         relations = []
         for n in doc.noun_chunks:
             noun_chunk = []
-            if not negation_detector(n.root):
-                for t in n:
-                    if not t.is_stop:
-                        noun_chunk.append(t.text.lower())
-                relations.append((entity_id, "has", " ".join(noun_chunk)))
+            if not noun_chunk_ent_detector(n) and not negation_detector(
+                n.root
+            ):
+                for t in find_composite_noun_chunk(n):
+                    noun_chunk.append(t.text.lower())
+                relations.append(
+                    (entity_id, "features the theme", " ".join(noun_chunk))
+                )
         return relations
 
     @staticmethod
@@ -76,12 +120,14 @@ class RelationExtractor:
             if not negation_detector(t):
                 if t.pos_ in ("ADJ"):
                     relations.append((entity_id, "is", t.text.lower()))
+                """
                 if t.pos_ in ("ADV"):
                     verb = verb_extractor(t)
                     if verb is not None:
                         relations.append(
                             (entity_id, verb.lower(), t.text.lower())
                         )
+                """
         return relations
 
     @staticmethod
@@ -89,6 +135,10 @@ class RelationExtractor:
         filtered_relations = []
         for relation in relations:
             if relation[1] == "" or relation[2] == "":
+                continue
+            elif stop_phrase_detector(relation[2]):
+                continue
+            elif removal_word_detector(relation[2]):
                 continue
             else:
                 filtered_relations.append(relation)
